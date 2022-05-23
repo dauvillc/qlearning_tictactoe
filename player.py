1d4a0596798fb4f48145ace32d3154629f579c90
Clément Dauvilliers <clement.dauvilliers@grenoble-inp.org>
2022-05-23 15:01:01 +0200

    qlearning act() fix

--- player.py
+++ player.py
@@ -1,12 +1,11 @@
 import numpy as np
 
-
 class Player:
     """
     Base class for both types of players (QLearning, DQN).
     """
 
-    def __init__(self, player='X', epsilon=0.05, seed=666):
+    def __init__(self, epsilon, player='X', seed=666):
         self.player = player
         self.epsilon = epsilon
 
@@ -32,11 +31,11 @@ class Player:
         for i in range(9):
             pos = (int(i / 3), i % 3)
             if grid[pos] == 0:
-                avail.append(pos)
+                avail.append(i)
         return avail
 
     def randomMove(self, grid):
         """ Chose a random move from the available options. """
         avail = self.empty(grid)
 
-        return avail[self.rng_.integers(0, len(avail))]
+        return self.rng_.choice(avail)
